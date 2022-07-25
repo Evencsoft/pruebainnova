c078fb7dbd1596503ac2ba96c093321efdb8bfcb
Francisco Lizcano <flizcanor@icloud.com>
2022-07-25 13:28:15 -0500

    ajustes ejemplo muestra recurso 6

--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -123,6 +123,13 @@
                     figure
                       img(src='@/assets/curso/temas/sliders/sliderB_04.svg')
 
+            .row.justify-content-center
+              .col-auto
+                a.anexo(href="https://drive.google.com/file/d/1SphPoasXbUQjw9fD5I3fgugAeEQWRScZ/view?usp=sharing" target="_blank")
+                  .anexo__icono
+                    img(src="@/assets/template/icono-pdf.svg")
+                  .anexo__texto
+                    p Formato instruccional - muestra
 
     separador
 
@@ -241,55 +248,21 @@
                     figure
                       img(src='@/assets/curso/temas/sliders/pasos_10.svg', alt='Texto que describa la imagen')
 
+            //.row.justify-content-center
+              .col-auto
+                a.anexo(href="https://drive.google.com/file/d/1SphPoasXbUQjw9fD5I3fgugAeEQWRScZ/view?usp=sharing" target="_blank")
+                  .anexo__icono
+                    img(src="@/assets/template/icono-pdf.svg")
+                  .anexo__texto
+                    p Formato instruccional - muestra
 
-    .titulo-segundo.color-acento-contenido
-      h2 Ejemplos destacados
 
-    #t_6_1
-    h3.mb-3 #[i.fas.fa-angle-double-right.txt--orange.pe-2]6.1 Slider simple
-    .tarjeta.tarjeta--azul.p-4.mb-5
-      SlyderA
-        .row.justify-content-center.align-items-center.my-lg-4
-          .col-md-6.mb-4.mb-md-0
-            p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+    separador
 
-          .col-md-6
-            figure
-              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
-              figcaption Leyenda de la imagen
+    #t_6_3.titulo-segundo.color-acento-contenido
+      h2 6.3 Slide navegación con títulos.
 
-        .row.justify-content-center.align-items-center.my-lg-4
-          .col-md-6
-            figure
-              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
-              figcaption Leyenda de la imagen
-
-          .col-md-6.mb-4.mb-md-0
-            p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
-
-    hr.my-4
-    .tarjeta.tarjeta--azul.p-4.mb-5
-      SlyderA(tipo="b")
-        .row.justify-content-center.align-items-center.my-lg-4
-          .col-md-6.mb-4.mb-md-0
-            p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
-
-          .col-md-6
-            figure
-              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
-              figcaption Leyenda de la imagen
-
-        .row.justify-content-center.align-items-center.my-lg-4
-          .col-md-6
-            figure
-              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
-              figcaption Leyenda de la imagen
-
-          .col-md-6.mb-4.mb-md-0
-            p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
-
-    #t_6_2
-    h3.mb-3 #[i.fas.fa-angle-double-right.txt--orange.pe-2] 6.2 Slider con Titulo
+    p.mb-4 Es para textos un poco más extensos que en el tipo anterior y la imagen puede ser más ilustrativa. Ideal para explicar fases, etapas, acontecimientos (línea de tiempo) o procedimientos. Cada diapositiva tendrá un nombre referente a su contenido. Se recomienda usar máximo 10 pasos y mínimo 4.
 
     .tarjeta.tarjeta--gris.p-4.mb-5
       PasosB.color-acento-contenido
@@ -310,52 +283,126 @@
           .col-md-6.mb-4.mb-md-0
             p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
 
-    hr.my-4
-    .tarjeta.tarjeta--gris.p-4.mb-5
-      LineaTiempoC.color-acento-contenido(text-small)
-        .row(titulo="Titulo 3")
-          .col-md-6.mb-4.mb-md-0
-            p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+      hr.my-4
+      .tarjeta.tarjeta--gris.p-4.mb-5
+        LineaTiempoC.color-acento-contenido(text-small)
+          .row(titulo="Titulo 3")
+            .col-md-6.mb-4.mb-md-0
+              p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
 
-          .col-md-6
-            figure
-              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
-              figcaption Leyenda de la imagen
-
-
-        .row(titulo="Titulo 4")
-          .col-md-6
-            figure
-              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
-              figcaption Leyenda de la imagen
-
-          .col-md-6.mb-4.mb-md-0
-            p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
-
-    #t_6_3
-    h3.mb-3 #[i.fas.fa-angle-double-right.txt--orange.pe-2] 6.2 Numeral o pasos
-
-    .tarjeta.tarjeta--gris.p-4.mb-5
-      PasosB.color-acento-contenido
-        .row(titulo="")
-          .col-md-6.mb-4.mb-md-0
-            p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
-
-          .col-md-6
-            figure
-              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
-              figcaption Leyenda de la imagen
-
-        .row(titulo="")
-          .col-md-6
-            figure
-              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
-              figcaption Leyenda de la imagen
-          .col-md-6.mb-4.mb-md-0
-            p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+            .col-md-6
+              figure
+                img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
+                figcaption Leyenda de la imagen
 
 
+          .row(titulo="Titulo 4")
+            .col-md-6
+              figure
+                img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
+                figcaption Leyenda de la imagen
 
+            .col-md-6.mb-4.mb-md-0
+              p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+
+
+    .row
+      .col-lg-12.order-2.order-lg-1
+        TabsA.color-acento-botones
+          .tarjeta.clr--gris.p-4(titulo="Requerimientos de información")
+            ul.lista-ul.mb-0
+              li
+                i.fas.fa-angle-right.txt--orange
+                | Título o subtítulo de la temática que se aborda
+              li
+                i.fas.fa-angle-right.txt--orange
+                | Colocar una breve descripción del tema que se aborda en el slide
+              li
+                i.fas.fa-angle-right.txt--orange
+                | Colocar el texto que va en cada diapositiva según el formato instruccional
+
+
+            p.mb-4 A continuación se presenta el formato instruccional el cual cuenta con las especificaciones necesarias para la producción del slide de diapositivas
+
+            .tarjeta.color-primario.p-3
+              .row.justify-content-around.align-items-center
+                .col-auto
+                  img(src="@/assets/template/document.svg").img65
+                .col
+                  .row.justify-content-between.align-items-center
+                    .col.mb-3.mb-sm-0
+                      h4.mb-1 Formato instruccional - Slide diapositivas (Títulos)
+                      p.mb-0.text-small Formato de Diseño Instruccional para la creación de infografías interactivas.
+                    .col-sm-auto
+                      a.boton.color-acento-botones(href="https://docs.google.com/document/d/1g6fMSTR-6mclIg_S-JgSTZyWKD894kKfSDSaNY63G7c/edit?usp=sharing" target="_blank")
+                        span Descargar
+                        i.fas.fa-file-download
+
+          .tarjeta.color-acento-botones--borde.p-4(titulo="Ejemplo - 1")
+            LineaTiempoC.color-secundario(text-small)
+              .row.align-items-center(titulo="Prehistoria").p-4
+                .col-lg-5
+                  figure
+                    img(src='@/assets/curso/temas/sliders/LT_01.png' alt='Hombres primitivos con un mamut').mb-4
+                .col-lg-7
+                  h4 La tecnología en la prehistoria
+                  p Inicialmente el ser humano desarrolló avances tecnológicos como respuesta a la supervivencia y para eso elaboró instrumentos para cazar, protegerse de los animales, etc.
+
+              .row.align-items-center(titulo="Edad Antigua").p-4
+                .col-lg-5
+                  figure
+                    img(src='@/assets/curso/temas/sliders/LT_02.png' alt='Hombres primitivos con un mamut').mb-4
+                .col-lg-7
+                  h4 La tecnología en la Edad Antigua
+                  p En esta etapa de la historia se generó la escritura, sin embargo, las primeras plumas comenzaron a usarse en el año 3000 a.C. por la antigua civilización egipcia para la elaboración de papiros y otros manuscritos.
+                  p Por otra parte, es realmente asombroso que culturas antiguas como la egipcia y otras que surgieron miles de años antes de Cristo, tengan evidencia de enormes avances en ingeniería para la época y con construcciones de mucho nivel como los que se reflejan en las pirámides de Egipto.
+
+              .row.align-items-center(titulo="Edad Media").p-4
+                .col-lg-5
+                  figure
+                    img(src='@/assets/curso/temas/sliders/LT_03.png' alt='Hombres primitivos con un mamut').mb-4
+                .col-lg-7
+                  h4 La tecnología en la Edad Media
+                  p El ser humano empezó a emplear máquinas simples, como molinos de viento, y máquinas a vapor que permitan sustituir la tracción animal y generaban más eficiencia y permitían el comercio entre lugares lejanos. Algunos inventos correspondientes a esta época son: los relojes, molinos de viento, la brújula, entre otros.
+                  p En esta edad es representativo la invención de la imprenta, con la  cual mejoró notablemente la comunicación entre las diferentes poblaciones.
+
+              .row.align-items-center(titulo="Edad Moderna").p-4
+                .col-lg-5
+                  figure
+                    img(src='@/assets/curso/temas/sliders/LT_04.png' alt='Hombres primitivos con un mamut').mb-4
+                .col-lg-7
+                  h4 La tecnología en la Edad Moderna
+                  p Esta época se caracteriza por la invención de artefactos como el telescopio, microscopio, reloj de péndulo, lápiz, calculadora, entre otros.
+                  h5 Revoluciones industriales
+                  p El conocimiento de la física y la ciencia en general permitió que los avances siguieran siendo representativos a tal punto que se han vivido cuatro revoluciones industriales que influyeron no solo en la productividad, sino en el tipo de empleos y de emprendimientos que surgieron.
+
+              .row.align-items-center(titulo="Contemporánea").p-4
+                .col-lg-5
+                  figure
+                    img(src='@/assets/curso/temas/sliders/LT_05.png' alt='Hombres primitivos con un mamut').mb-4
+                .col-lg-7
+                  h4 La tecnología en la Edad Contemporánea
+                  h5 Transición de lo análogo a lo digital
+                  p Ahora pensemos por un momento en lo que sucedía en los años 90: si una persona quería entregar un trabajo escrito, debía hacerlo a mano o utilizar una máquina de escribir, esta última generó un impacto positivo para quien tenía dificultades al leer diversas caligrafías; pero si en la actualidad un joven utiliza esta máquina de seguro encontrará dificultades impensables hoy, por ejemplo si cometemos un error al escribir un texto hoy, simplemente lo borramos o presionamos “deshacer” en un software, pero con una máquina de escribir simplemente se arrancaba la hoja y había que empezar de nuevo.
+
+              .row.align-items-center(titulo="Edad digital").p-4
+                .col-lg-4
+                  figure
+                    img(src='@/assets/curso/temas/sliders/LT_06.png' alt='Hombres primitivos con un mamut').mb-4
+                .col-lg-8
+                  h4 La tecnología en la Edad Contemporánea
+                  h5 Tecnología móvil
+                  p Cuando los microchips empezaron a ser más conocidos y usados, la electrónica se extendió de manera más rápida y surgieron computadores, localizadores, elementos digitales y poco a poco fueron avanzando hasta llegar a tener diversos tamaños como por ejemplo en teléfonos donde se integran tecnologías de audio y video, con procesadores de textos, sistemas operativos que permiten realizar procesos para los cuales antes se necesitaban varios dispositivos.
+                  p Hoy un teléfono nos permite escanear un documento y enviarlo por correo en poco tiempo pero, además, permite que emprendedores de esta era puedan realizar un diseño de un banner con una simple aplicación móvil, subirlo a una red social o enviarlo por diversos sistemas de mensajería a muchos clientes.
+                  p Estamos en una era digital donde la información y la tecnología están al alcance de todos y es importante reflexionar sobre los aprendizajes que se vienen en los próximos años.
+
+            .row.justify-content-center
+              .col-auto
+                a.anexo(href="https://drive.google.com/file/d/1oFq1y5Qyzo-C9TucftYqYORPB3ogW5Dy/view?usp=sharing" target="_blank")
+                  .anexo__icono
+                    img(src="@/assets/template/icono-pdf.svg")
+                  .anexo__texto
+                    p Formato instruccional - muestra
 
 </template>
 
