65d2b8fd8f6d87df7200c3b9f22262e7f2a3d637
Francisco Lizcano <flizcanor@icloud.com>
2022-07-25 04:02:57 -0500

    Ajustes plantilla

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -7,55 +7,73 @@
         span 1
       h1 Infografía estática
 
-    .row.justify-content-center.align-items-center.mb-4
+    .row.justify-content-center.align-items-center.mb-5
       .col-lg-6
         figure
           img(src="@/assets/curso/temas/infografia/infografia.svg", alt="alt")
       .col-lg-6
         .crd.clr-paleYellow.p-4.mb-4
-          p.mb-0 Las infografías son datos presentados con el auxilio de recursos visuales. #[strong Siempre que usamos imágenes para explicar datos, tenemos un infografía]. 
+          p.mb-0 La #[strong Infografía estática] se presentará como una sola pieza gráfica que se auto sustenta a partir de texto corto, no tiene botones de interaccion.
 
-        .tarjeta.color-primario.p-3
-          .row.justify-content-around.align-items-center
-            .col-auto
-              img(src="@/assets/template/document.svg").img65
-            .col
-              .row.justify-content-between.align-items-center
-                .col.mb-3.mb-sm-0
-                  h4.mb-1 Formato instruccional - Infografía estática
-                  p.mb-0.text-small Formato de Diseño Instruccional para la creación de infografías estáticas.
-                .col-sm-auto
-                  a.boton.color-acento-botones(:href="obtenerLink('/downloads/prueba.pdf')" target="_blank")
-                    span Descargar
-                    i.fas.fa-file-download  
+        p.mb-4 Se utilizará en el momento que se desee brindar una explicación concreta de un tema a partir de datos específicos y secuenciales, apoyados, cada uno, con una imagen. Por ejemplo: Resúmenes, caracterizaciones, estadísticas, procesos, tipificaciones, fases, conclusiones.
+
+        p.mb-4 No deben ser secuencias muy extensas, deben contar con un objetivo definido y todo el contenido enfocado a este.
+
 
     .row
       .col-lg-12.order-2.order-lg-1
-        TabsA.color-acento-botones       
+        TabsA.color-acento-botones
           .tarjeta.clr--gris.p-4(titulo="Requerimientos de información")
-            ul.lista-ul.mb-0
-              li 
+            ul.lista-ul.mb-4
+              li
                 i.fas.fa-angle-right.txt--orange
                 | Título corto
-              
+              li
+                i.fas.fa-angle-right.txt--orange
+                | <b>Texto introductorio:</b> &nbsp;colocar una breve descripción del tema que se aborda en la infografía.
+              li
+                i.fas.fa-angle-right.txt--orange
+                | <b>Imagen de referencia:</b> &nbsp;Describir y/o colocar una imagen de referencia, de la infografía solicitada.
+              li
+                i.fas.fa-angle-right.txt--orange
+                | Incluir cualquier observación o solicitud específica frente al diseño gráfico de la infografía.
+
+
+            p.mb-4 A continuación se presenta el formato instruccional el cual cuenta con las especificaciones necesarias para la producción de la infografía.
+
+
+            .tarjeta.color-primario.p-3
+              .row.justify-content-around.align-items-center
+                .col-auto
+                  img(src="@/assets/template/document.svg").img65
+                .col
+                  .row.justify-content-between.align-items-center
+                    .col.mb-3.mb-sm-0
+                      h4.mb-1 Formato instruccional - Infografía estática
+                      p.mb-0.text-small Formato de Diseño Instruccional para la creación de infografías estáticas.
+                    .col-sm-auto
+                      a.boton.color-acento-botones(href="https://docs.google.com/document/d/1wD3oByzOhsH4c0oWnUb4DWNBc0hmCOhrMDWG0-nCtyo/edit?usp=sharing" target="_blank")
+                        span Descargar
+                        i.fas.fa-file-download
+
           .tarjeta.clr--gris.p-4(titulo="Recomendaciones del diseñador")
             p #[strong 1) Definir un tema.]
               ul.lista-ul.ps-4
-                li 
+                li
                   i.fas.fa-angle-right.txt--orange
                   | Explicación de un concepto.
-                li 
+                li
                   i.fas.fa-angle-right.txt--orange
                   | Explicación de una tecnología o proceso.
-                li 
+                li
                   i.fas.fa-angle-right.txt--orange
                   | Representación de datos estadísticos, biográficos, geográficos, etc.
-                li 
+                li
                   i.fas.fa-angle-right.txt--orange
                   | Resumen de un documento.
 
               .hr--line
-              p.mb-0 #[strong 2) Elegir un título atractivo.]<br> Se debe captar la atención del lector por medio de títulos cortos y llamativos que representen correctamente el contenido abordado. 
+              p.mb-0 #[strong 2) Elegir un título atractivo.]<br> Se debe captar la atención del lector por medio de títulos cortos y llamativos que representen correctamente el contenido abordado.
               .hr--line
               p.mb-0 #[strong 3) Utilizar oraciones cortas y objetivas.]
               .hr--line
@@ -67,10 +85,21 @@
               .hr--line
               p.mb-0 #[strong 7) Los gráficos, diagramas, iconos y demás elementos planteados deben ser claros y específicos.]
 
-    separador        
+    separador
 
-    .titulo-segundo.color-acento-contenido
-      h2 Ejemplos destacados 
+    h3.mb-5 Ejemplos
+
+    .titulo-sexto.color-acento-contenido
+      h5 Ejemplo 01
+      span Infografía como imagen dentro del contenido
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-10
+        img(src="@/assets/curso/temas/infografia/info1.png")
+
+    .titulo-sexto.color-acento-contenido
+      h5 Ejemplo 02
+      span Infografía descargable en pdf, con un título y texto introductorio.
 
     .tarjeta.color-acento-botones.p-4.p-md-5.mb-3
       .row.justify-content-around.align-items-center
@@ -80,22 +109,22 @@
         .col-md.col-lg-6
           h3 Psicología de la moda: sexualidad, ropa mágica, ropa malévola y ropa neurótica.
           p.mb-4 La conexión entre el vestuario y la persona que lo porta es lo que llamamos proceso efectivo de comunicación de las marcas, direccionado desde la psicología de la moda, lo invitamos a profundizar en esta temática consultando el siguiente recurso infografico.
-          
+
           a.boton.color-acento-contenido.texto-blanco(:href="obtenerLink('/downloads/Infografia_Psicologia_de_moda.pdf')" target="_blank" type="application/pdf")
             span Descargar
-            i.fas.fa-file-download   
+            i.fas.fa-file-download
 
-    .row.justify-content-center
+    //.row.justify-content-center
       .col-auto
         a.anexo(:href="obtenerLink('/downloads/Psicologia_Moda.docx')" target="_blank")
           .anexo__icono
             img(src="@/assets/template/icono-doc.svg")
           .anexo__texto
-            p Formato instruccional - muestra      
+            p Formato instruccional - muestra
 
 
-    
-      
+
+
 </template>
 
 <script>
